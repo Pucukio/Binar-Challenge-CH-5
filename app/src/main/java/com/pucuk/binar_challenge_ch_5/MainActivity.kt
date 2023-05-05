b81364f48c2bc858e8ab6cf3ba437632ee21f336
Pucukio <pucuk4us@gmail.com>
2023-05-05 12:11:59 +0700

    Fixxing UI Screen

--- app/src/main/java/com/pucuk/binar_challenge_ch_5/MainActivity.kt
+++ app/src/main/java/com/pucuk/binar_challenge_ch_5/MainActivity.kt
@@ -2,10 +2,16 @@ package com.pucuk.binar_challenge_ch_5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.pucuk.binar_challenge_ch_5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
